19193382b28b3d83927a4e0eeda41ca3be98e7eb
aswinshenoy <aswinshenoy65@gmail.com>
2018-11-12 02:59:17 +0530

    landing page

--- Portal/projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url('projects', views.projectlist, name="projects"),
-]
--- Portal/projects/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from . import models
-
-# Create your views here.
-def projectlist(request):
-     projects = models.project.objects.all()
-     return render(request, 'project/list.html', {'projects': projects})
